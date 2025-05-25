b05723f79ece25bcd916c9e09bbeef2feb7b32e1
Sohag00879 <shishirsohagislam@gmail.com>
2025-05-25 06:35:47 +0600

    pricing page done

---
+++ src/components/Pricing/PricingCardMonth.tsx
@@ -0,0 +1,37 @@
+const features = [
+  'Zero Swap Fee',
+  'Boosted listings',
+  'Faster matching',
+  'Priority visibility'
+];
+
+const PricingCardMonth = () => {
+  return (
+    <div className="max-w-sm w-full rounded-xl shadow-lg overflow-hidden p-8 text-center flex flex-col justify-between h-[457px]  bg-warning-200">
+      <div className="upper">
+        <h1 className="text-[36px] font-bold text-gray-800 mb-6">Premium Membership</h1>
+        <ul className="text-left pl-4">
+          {features.map((item, index) => (
+            <li key={index} className="flex items-center gap-3 mb-2">
+              <span className="w-[10px] h-[10px] bg-red-500 rounded-full flex-shrink-0"></span>
+              <span>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="under">
+        <div className="mb-8">
+          <span className="text-4xl font-bold text-gray-900">$50</span>
+          <span className="text-[16px] font-medium text-gray-600">/month</span>
+        </div>
+
+        <button className="bg-warning-500 hover:bg-yellow-600 text-white font-bold py-3 px-20 rounded-md transition duration-200">
+          Upgrade Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PricingCardMonth;
