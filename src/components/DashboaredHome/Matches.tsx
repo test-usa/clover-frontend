3cd4b35a8302d6d8444129616f7013e208e84303
Sohag00879 <shishirsohagislam@gmail.com>
2025-05-26 03:38:24 +0600

    dashboard home completed

---
+++ src/components/DashboaredHome/Matches.tsx
@@ -0,0 +1,80 @@
+import ReUsableCard from '../Reusable/ReUsableCard'
+
+export interface MatchCardProps {
+
+    avatarSrc: string;
+
+    name: string;
+
+    rating: number;
+
+    location: string;
+
+    status: "Available" | "Busy";
+
+    offerSkill: string;
+
+    exchangeSkill: string;
+
+    verifiedType?: "blue" | "orange" | "none";
+
+}
+
+const Matches = () => {
+
+    const users: MatchCardProps[] = [
+        {
+            avatarSrc: "https://www.shutterstock.com/image-photo/smiling-african-american-millennial-businessman-600nw-1437938108.jpg",
+            name: "Eric Yates",
+            rating: 4.7,
+            location: "London, UK",
+            status: "Available",
+            offerSkill: "Graphic Design",
+            exchangeSkill: "Frontend Development",
+            verifiedType: "blue",
+        },
+        {
+            avatarSrc: "https://www.shutterstock.com/image-photo/portrait-happy-young-asian-woman-600nw-1858164334.jpg",
+            name: "Neha Mayumi",
+            rating: 4.9,
+            location: "Tokyo, Japan",
+            status: "Busy",
+            offerSkill: "Content Writing",
+            exchangeSkill: "SEO Optimization",
+            verifiedType: "orange",
+        },
+        {
+            avatarSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9F7aRc0I5KJxVfeKHxNg0T0PGtfzc59ixpA&s",
+            name: "Carlos Rivera",
+            rating: 4.6,
+            location: "Madrid, Spain",
+            status: "Available",
+            offerSkill: "Video Editing",
+            exchangeSkill: "Motion Graphics",
+            verifiedType: "none",
+        }
+    ];
+    return (
+        <>
+            <h1 className="text-[28px] font-bold mt-10 mb-6">Matches For You</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {users.map((item, idx) => (
+                    <ReUsableCard
+                        key={idx}
+                        avatarSrc={item.avatarSrc}
+                        name={item.name}
+                        rating={item.rating}
+                        location={item.location}
+                        status={item.status}
+                        offerSkill={item.offerSkill}
+                        exchangeSkill={item.exchangeSkill}
+                        verifiedType={item.verifiedType}
+                    />
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Matches
--- src/pages/Dashboard/DashboardHome.tsx
+++ src/pages/Dashboard/DashboardHome.tsx
@@ -1,16 +1,53 @@
+import Matches from "@/components/DashboaredHome/Matches";
 import Summary from "@/components/DashboaredHome/Summary";
+
+type TActivites = {
+  title: string;
+  time: string;
+}[]
+
+const activites: TActivites = [
+  {
+    title: 'You marked the swap Branding & Identity for Web Development with Eric Yates as completed.',
+    time: '1 hour ago'
+  },
+  {
+    title: 'You marked the swap Branding & Identity for Web Development with Eric Yates as completed.',
+    time: '1 hour ago'
+  },
+  {
+    title: 'You marked the swap Branding & Identity for Web Development with Eric Yates as completed.',
+    time: '1 hour ago'
+  },
+]
 
 const DashboardHome = () => {
   return (
-   <>
-    <div>
-      <h1 className="w-[700] text-[48px]">Hi Dwiky!</h1>
-      <p>Ready to swap skills?</p>
-    </div>
-    <div>
-      <Summary/>
-    </div>
-   </>
+    <>
+      <div>
+        <h1 className="w-[700] text-[48px]">Hi Dwiky!</h1>
+        <p>Ready to swap skills?</p>
+      </div>
+      <div>
+        <Summary />
+      </div>
+      <Matches />
+      <div>
+        <h1 className="text-[28px] font-bold mt-10 mb-6">Recent Activities</h1>
+        {
+          activites.map((item, i) => (
+            <div key={i} className="mb-5">
+              <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-1 sm:gap-4">
+                <p className="text-base text-typo-600">{item.title}</p>
+                <p className="text-base text-typo-600">{item.time}</p>
+              </div>
+              <hr className="text-gray-200 mt-2" />
+            </div>
+
+          ))
+        }
+      </div>
+    </>
   );
 };
 
